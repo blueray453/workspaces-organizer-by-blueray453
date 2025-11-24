456c3d0d6a7be6b675d12e77682e702892558fc3
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-11-24 10:01:17 +0600

    Alhamdulillah, RESTORE TO WORKING VERSION

--- extension.js
+++ extension.js
@@ -31,6 +31,9 @@ class WindowPreview extends St.Button {
         });
 
         this._hoverPreview = null;
+        this._showPreviewTimeout = null;
+        this._hidePreviewTimeout = null;
+        this._windowDestroyId = 0;
 
         this._delegate = this;
         DND.makeDraggable(this, { restoreOnSuccess: true });
@@ -59,16 +62,6 @@ class WindowPreview extends St.Button {
                 closeItem.connect('activate', () => this._window.delete(0));
                 menu.addMenuItem(closeItem);
 
-                let activateItem = new PopupMenu.PopupMenuItem(`Activate ${this._window.title}`);
-
-                activateItem.connect('activate', () => {
-                    let win_workspace = this._window.get_workspace();
-                    // Here global.get_current_time() instead of 0 will also work
-                    win_workspace.activate_with_focus(this._window, 0);
-                });
-
-                menu.addMenuItem(activateItem);
-
                 let closeAllItem = new PopupMenu.PopupMenuItem(`Close all windows on workspace ${this._window.get_workspace().index()}`);
                 menu.addMenuItem(closeAllItem);
 
@@ -89,92 +82,49 @@ class WindowPreview extends St.Button {
         });
 
         // Connect hover signals
-        this._enterEventId = this.connect('enter-event', () => this._showHoverPreview());
-        this._leaveEventId = this.connect('leave-event', () => this._hideHoverPreview());
-        this._destroyEventId = this.connect('destroy', () => {
-            this._window.disconnect(this._wmClassChangedId);
-            this._window.disconnect(this._mappedId);
-            this._hideHoverPreview();
-        });
-        this._wsChangedId = WorkspaceManager.connect(
-            'workspace-switched',
-            () => this._hideHoverPreview()
-        );
-
+        this.connect('enter-event', () => this._showHoverPreview());
+        this.connect('leave-event', () => this._hideHoverPreview());
+        this.connect('destroy', () => this._hideHoverPreview());
     }
 
     _showHoverPreview() {
-        if (!this._window || this._hoverPreview || this._hoverTimeout) return;
+        if (!this._window) return;
 
-        this._hoverTimeout = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 300, () => {
-            this._hoverTimeout = null;
+        const windowActor = this._window.get_compositor_private();
+        if (!windowActor) return;
 
-            const windowActor = this._window.get_compositor_private();
-                if (!windowActor) return GLib.SOURCE_REMOVE;
+        // const allocation = this.get_allocation_box();
+        // const actorWidth = allocation.get_width();
+        const actorWidth = this.get_width();
+        journal(`actorWidth : ${actorWidth}`);
+        const [actorX, actorY] = this.get_transformed_position();
 
-            // const allocation = this.get_allocation_box();
-            // const actorWidth = allocation.get_width();
-            const actorWidth = this.get_width();
-            journal(`actorWidth : ${actorWidth}`);
-            const [actorX, actorY] = this.get_transformed_position();
+        const windowFrame = this._window.get_frame_rect();
+        const windowWidth = windowFrame.width;
+        const windowHeight = windowFrame.height;
 
-            const windowFrame = this._window.get_frame_rect();
-            const windowWidth = windowFrame.width;
-            const windowHeight = windowFrame.height;
+        const aspectRatio = windowWidth / windowHeight;
 
-            const aspectRatio = windowWidth / windowHeight;
+        const previewHeight = 600; // fixed
+        const previewWidth = previewHeight * aspectRatio;
 
-            const previewHeight = 600; // fixed
-            const previewWidth = previewHeight * aspectRatio;
+        // Directly above the actor (no gap)
+        const previewX = actorX + (actorWidth - previewWidth) / 2;
+        const previewY = actorY - previewHeight - 20; // 20px gap above window
 
-            // Directly above the actor (no gap)
-            const previewX = actorX + (actorWidth - previewWidth) / 2;
-            const previewY = actorY - previewHeight - 40; // 20px gap above window
-
-            this._hoverPreview = new Clutter.Clone({
-                source: windowActor,
-                x: previewX,
-                y: previewY,
-                width: previewWidth,
-                height: previewHeight,
-                reactive: false // ensures it does not block hover leave
-            });
-
-            // Main.layoutManager.addChrome(this._hoverPreview);
-
-            this._hoverPreview.opacity = 0;
-            Main.layoutManager.addChrome(this._hoverPreview);
-            this._hoverPreview.ease({
-                opacity: 255,
-                duration: 600,
-                mode: Clutter.AnimationMode.EASE_OUT_QUAD,
-            });
-            return GLib.SOURCE_REMOVE;
+        this._hoverPreview = new Clutter.Clone({
+            source: windowActor,
+            x: previewX,
+            y: previewY,
+            width: previewWidth,
+            height: previewHeight
         });
+
+        Main.layoutManager.addChrome(this._hoverPreview);
     }
 
     _hideHoverPreview() {
-
-        if (this._hoverTimeout) {
-            GLib.source_remove(this._hoverTimeout);
-            this._hoverTimeout = null;
-        }
-
-        if (!this._hoverPreview) return;
-
-        // this._hoverPreview.ease({
-        //     opacity: 0,
-        //     duration: 300,
-        //     mode: Clutter.AnimationMode.EASE_IN_QUAD,
-        //     onComplete: () => {
-        //         Main.layoutManager.removeChrome(this._hoverPreview);
-        //         this._hoverPreview.destroy();
-        //         this._hoverPreview = null;
-        //     }
-        // });
-
         if (this._hoverPreview) {
-            Main.layoutManager.removeChrome(this._hoverPreview);
             this._hoverPreview.destroy();
             this._hoverPreview = null;
         }
@@ -204,44 +154,9 @@ class WindowPreview extends St.Button {
     }
 
     destroy() {
-        // Disconnect preview signals
-        if (this._enterEventId) {
-            this.disconnect(this._enterEventId);
-            this._enterEventId = null;
-        }
-
-        if (this._leaveEventId) {
-            this.disconnect(this._leaveEventId);
-            this._leaveEventId = null;
-        }
-
-        if (this._destroyEventId) {
-            this.disconnect(this._destroyEventId);
-            this._destroyEventId = null;
-        }
-
-        if (this._hoverPreview) {
-            this._hideHoverPreview();
-        }
-
-        /* disconnect window signal: wm-class */
-        if (this._wmClassChangedId && this._window) {
-            this._window.disconnect(this._wmClassChangedId);
-            this._wmClassChangedId = null;
-        }
-
-        /* disconnect window signal: mapped */
-        if (this._mappedId && this._window) {
-            this._window.disconnect(this._mappedId);
-            this._mappedId = null;
-        }
-
-        /* disconnect workspace-changed */
-        if (this._wsChangedId && WorkspaceManager) {
-            WorkspaceManager.disconnect(this._wsChangedId);
-            this._wsChangedId = null;
-        }
-
+        WorkspaceManager.disconnect(this._focusChangedId);
+        this._window.disconnect(this._wmClassChangedId);
+        this._window.disconnect(this._mappedId);
         super.destroy();
     }
 }
@@ -254,7 +169,7 @@ class WorkspaceThumbnail extends St.Button {
         GObject.registerClass(this);
     }
 
-    constructor(workspace) {
+    constructor(index) {
         super({
             style_class: 'workspace',
             x_expand: true,
@@ -269,22 +184,26 @@ class WorkspaceThumbnail extends St.Button {
 
         this.set_child(this._windowsBox);
 
+        this._index = index;
         this._delegate = this; // needed for DND
 
         this._windowPreviews = new Map();
         this._addWindowTimeoutIds = new Map();
 
-        this._workspace = workspace;
+        this._workspace = WorkspaceManager.get_workspace_by_index(index);
 
         this.connect('button-press-event', (actor, event) => {
             let button = event.get_button();
 
             if (button === Clutter.BUTTON_PRIMARY) { // left click
-                this._workspace.activate(0);
+                let ws = WorkspaceManager.get_workspace_by_index(this._index);
+                if (ws)
+                    ws.activate(0);
                 return Clutter.EVENT_STOP; // prevent default
             }
 
             if (button === Clutter.BUTTON_SECONDARY) { // right click
+                journal(`Right click detected on workspace ${this._index}!`);
                 let windows = this._workspace.list_windows().filter(w =>
                     w.get_window_type() === 0
                 );
@@ -292,7 +211,7 @@ class WorkspaceThumbnail extends St.Button {
                 const windowCount = windows.length;
 
                 if (windowCount === 0) {
-                    return Clutter.EVENT_STOP; // Fix: Return STOP to prevent menu creation
+                    return
                 }
 
                 let menu = new PopupMenu.PopupMenu(this, 0.0, St.Side.TOP, 0);
@@ -303,7 +222,7 @@ class WorkspaceThumbnail extends St.Button {
                 manager.addMenu(menu);
                 Main.uiGroup.add_child(menu.actor);
 
-                let closeAllItem = new PopupMenu.PopupMenuItem(`Close all windows on workspace ${this._workspace.index()}`);
+                let closeAllItem = new PopupMenu.PopupMenuItem(`Close all windows on workspace ${this._index}`);
                 menu.addMenuItem(closeAllItem);
 
                 closeAllItem.connect('activate', () => {
@@ -318,7 +237,7 @@ class WorkspaceThumbnail extends St.Button {
             }
 
             // For left click, let the default handler work
-            // return Clutter.EVENT_PROPAGATE;
+            return Clutter.EVENT_PROPAGATE;
         });
 
         this._windowAddedId = this._workspace.connect('window-added',
@@ -428,7 +347,7 @@ class WorkspaceThumbnail extends St.Button {
         let monitorIndex = Main.layoutManager.findIndexForActor(this);
         if (monitorIndex !== window.get_monitor())
             window.move_to_monitor(monitorIndex);
-        window.change_workspace(this._workspace);
+        window.change_workspace_by_index(this._index, false);
     }
 
     // Explicitly cancel main loop sources without destroying the actor
@@ -438,10 +357,6 @@ class WorkspaceThumbnail extends St.Button {
         }
         this._addWindowTimeoutIds.clear();
     }
-
-    // on_clicked() {
-    //     this._workspace.activate(0);
-    // }
 
     destroy() {
         this._workspace.disconnect(this._windowAddedId);
@@ -467,16 +382,21 @@ class WorkspaceIndicator extends PanelMenu.Button {
     constructor() {
         super(0.0, _('Workspace Indicator'));
 
-        this.add_style_class_name('workspace-indicator-button');
-
         let container = new St.Widget({
             layout_manager: new Clutter.BinLayout(),
             x_expand: true,
             y_expand: true,
-            reactive: true  // removes :insensitive
+        });
+        this.add_child(container);
+
+        this._currentWorkspace = WorkspaceManager.get_active_workspace_index();
+        this._statusLabel = new St.Label({
+            style_class: 'panel-workspace-indicator',
+            y_align: Clutter.ActorAlign.CENTER,
+            text: this._labelText(),
         });
 
-        this.add_child(container);
+        container.add_child(this._statusLabel);
 
         this._thumbnailsBox = new St.BoxLayout({
             style_class: 'panel-workspace-indicator-box',
@@ -487,50 +407,117 @@ class WorkspaceIndicator extends PanelMenu.Button {
 
         container.add_child(this._thumbnailsBox);
 
+        this._workspacesItems = [];
+        this._workspaceSection = new PopupMenu.PopupMenuSection();
+        this.menu.addMenuItem(this._workspaceSection);
+
         this._workspaceManagerSignals = [
             WorkspaceManager.connect_after('notify::n-workspaces',
-                this._updateThumbnails.bind(this)),
+                this._nWorkspacesChanged.bind(this)),
             WorkspaceManager.connect_after('workspace-switched',
-                this._updateActiveThumbnail.bind(this)),
+                this._onWorkspaceSwitched.bind(this)),
+            WorkspaceManager.connect('notify::layout-rows',
+                this._onWorkspaceOrientationChanged.bind(this)),
         ];
 
+        this._createWorkspacesSection();
         this._updateThumbnails();
-        // this._workspaceSection = new PopupMenu.PopupMenuSection();
-        // this.menu.addMenuItem(this._workspaceSection);
-        // this._createWorkspacesSection();
+        this._onWorkspaceOrientationChanged();
     }
 
-    // _createWorkspacesSection() {
-    //     this._workspaceSection.removeAll();
-    //     this._workspacesItems = [];
-    //     this._currentWorkspace = WorkspaceManager.get_active_workspace_index();
+    destroy() {
+        this.cleanupSources();
+        this._thumbnailsBox?.destroy();
 
-    //     let i = 0;
-    //     for (; i < WorkspaceManager.n_workspaces; i++) {
-    //         this._workspacesItems[i] = new PopupMenu.PopupMenuItem(Meta.prefs_get_workspace_name(i));
-    //         this._workspaceSection.addMenuItem(this._workspacesItems[i]);
-    //         this._workspacesItems[i].workspaceId = i;
+        for (let i = 0; i < this._workspaceManagerSignals.length; i++)
+            WorkspaceManager.disconnect(this._workspaceManagerSignals[i]);
 
-    //         if (i === this._currentWorkspace)
-    //             this._workspacesItems[i].setOrnament(PopupMenu.Ornament.DOT);
-    //     }
-    // }
+        Main.panel.set_offscreen_redirect(Clutter.OffscreenRedirect.ALWAYS);
+
+        super.destroy();
+    }
+
+    _onWorkspaceOrientationChanged() {
+        let vertical = WorkspaceManager.layout_rows === -1;
+        this.reactive = vertical;
+
+        this._statusLabel.visible = vertical;
+        this._thumbnailsBox.visible = !vertical;
+
+        // Disable offscreen-redirect when showing the workspace switcher
+        // so that clip-to-allocation works
+        Main.panel.set_offscreen_redirect(vertical
+            ? Clutter.OffscreenRedirect.ALWAYS
+            : Clutter.OffscreenRedirect.AUTOMATIC_FOR_OPACITY);
+    }
+
+    _onWorkspaceSwitched() {
+        this._currentWorkspace = WorkspaceManager.get_active_workspace_index();
+
+        this._updateMenuOrnament();
+        this._updateActiveThumbnail();
+
+        this._statusLabel.set_text(this._labelText());
+    }
+
+    _nWorkspacesChanged() {
+        this._createWorkspacesSection();
+        this._updateThumbnails();
+    }
+
+    _updateMenuOrnament() {
+        for (let i = 0; i < this._workspacesItems.length; i++) {
+            this._workspacesItems[i].setOrnament(i === this._currentWorkspace
+                ? PopupMenu.Ornament.DOT
+                : PopupMenu.Ornament.NONE);
+        }
+    }
 
     _updateActiveThumbnail() {
         let thumbs = this._thumbnailsBox.get_children();
         for (let i = 0; i < thumbs.length; i++) {
-            if (i === WorkspaceManager.get_active_workspace_index())
+            if (i === this._currentWorkspace)
                 thumbs[i].add_style_class_name('active');
             else
                 thumbs[i].remove_style_class_name('active');
         }
     }
 
+    _labelText(workspaceIndex) {
+        if (workspaceIndex === undefined) {
+            workspaceIndex = this._currentWorkspace;
+            return (workspaceIndex + 1).toString();
+        }
+        return Meta.prefs_get_workspace_name(workspaceIndex);
+    }
+
+    _createWorkspacesSection() {
+        this._workspaceSection.removeAll();
+        this._workspacesItems = [];
+        this._currentWorkspace = WorkspaceManager.get_active_workspace_index();
+
+        let i = 0;
+        for (; i < WorkspaceManager.n_workspaces; i++) {
+            this._workspacesItems[i] = new PopupMenu.PopupMenuItem(this._labelText(i));
+            this._workspaceSection.addMenuItem(this._workspacesItems[i]);
+            this._workspacesItems[i].workspaceId = i;
+            this._workspacesItems[i].label_actor = this._statusLabel;
+            this._workspacesItems[i].connect('activate', (actor, _event) => {
+                this._activate(actor.workspaceId);
+            });
+
+            if (i === this._currentWorkspace)
+                this._workspacesItems[i].setOrnament(PopupMenu.Ornament.DOT);
+        }
+
+        this._statusLabel.set_text(this._labelText());
+    }
+
     _updateThumbnails() {
         this._thumbnailsBox.destroy_all_children();
 
         for (let i = 0; i < WorkspaceManager.n_workspaces; i++) {
-            let thumb = new WorkspaceThumbnail(WorkspaceManager.get_workspace_by_index(i));
+            let thumb = new WorkspaceThumbnail(i);
             this._thumbnailsBox.add_child(thumb);
         }
         this._updateActiveThumbnail();
@@ -545,16 +532,11 @@ class WorkspaceIndicator extends PanelMenu.Button {
         }
     }
 
-    destroy() {
-        this.cleanupSources();
-        this._thumbnailsBox?.destroy();
-
-        for (let i = 0; i < this._workspaceManagerSignals.length; i++)
-            WorkspaceManager.disconnect(this._workspaceManagerSignals[i]);
-
-        Main.panel.set_offscreen_redirect(Clutter.OffscreenRedirect.ALWAYS);
-
-        super.destroy();
+    _activate(index) {
+        if (index >= 0 && index < WorkspaceManager.n_workspaces) {
+            let metaWorkspace = WorkspaceManager.get_workspace_by_index(index);
+            metaWorkspace.activate(0);
+        }
     }
 }
 
@@ -562,6 +544,8 @@ export default class TopNotchWorkspaces extends Extension {
     constructor(metadata) {
         super(metadata);
         this._indicator = null;
+        this._handles = [];
+        this._origUpdateSwitcher = null;
     }
 
     enable() {
