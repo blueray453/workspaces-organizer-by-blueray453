07c217293e2ee98de69f1ef3a3843d2bef7618b4
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2026-08-24 15:53:57 +0600

    Alhamdulillah, add local features instasd of calling gdmenu

--- extension.js
+++ extension.js
@@ -19,7 +19,7 @@ export default class TopNotchWorkspaces extends Extension {
     }
 
     enable() {
-        initLogging(this.uuid, 'both', false);
+        initLogging(this.uuid, 'both', true);
         journal(`Enabled`);
 
         const settings = this.getSettings(); // Reads settings-schema from metadata.json
